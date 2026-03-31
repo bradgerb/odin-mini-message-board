4d9d903018ef1258c08f397efac2051de000c58a
bradgerb <77023448+bradgerb@users.noreply.github.com>
2026-03-31 19:40:58 -0400

    Finish details update

--- controllers/detailsController.js
+++ controllers/detailsController.js
@@ -4,12 +4,17 @@ const CustomNotFoundError = require("../errors/CustomNotFoundError");
 const getDetails = async (req, res) => {
 
     const { detailID } = req.params;
+    const messages = await db.getMessages();
 
     if (!detailID) {
         throw new CustomNotFoundError("Message details not found");
     }
 
-  res.render('details', {detailID: detailID});
+    if (!messages) {
+        throw new CustomNotFoundError("Messages not found");
+    }
+
+  res.render('details', {detailID: detailID, messages: messages});
 };
 
 module.exports = { getDetails };
