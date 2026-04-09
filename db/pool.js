0c8db9e26d6d1062abb981883d5062add0bf4ab3
bradgerb <77023448+bradgerb@users.noreply.github.com>
2026-04-09 18:18:22 -0400

    Update host

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,13 @@
 const { Pool } = require("pg");
 
+const host = process.env.host;
 const dbUsername = process.env.dbUsername;
 const dbPassword = process.env.dbPassword;
 const dbName = process.env.dbName;
 const port = process.env.port;
 
 module.exports = new Pool({
-  host: "localhost",
+  host: `${host}`,
   user: `${dbUsername}`,
   database: `${dbName}`,
   password: `${dbPassword}`,
